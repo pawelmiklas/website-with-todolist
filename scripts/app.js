7b15edd11ccbbdad5cae243926328ef1d58019c2
pablomiklas@gmail.com <pablomiklas@gmail.com>
2020-03-01 19:34:03 +0100

    added hamburger menu

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navContent = document.querySelector('.nav-content__links');
+
+menuIcon.addEventListener('click', () => {
+  navContent.classList.toggle('nav-content-links--open');
+})
