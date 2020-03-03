7311f7d2dc47e6a448f932540b65967fc961475c
pablomiklas@gmail.com <pablomiklas@gmail.com>
2020-03-03 22:25:52 +0100

    chaged classes and added basic validation

--- scripts/app.js
+++ scripts/app.js
@@ -5,62 +5,76 @@ menuIcon.addEventListener('click', () => {
   navContent.classList.toggle('nav-content-links--open');
 })
 
+const validationField = (field) => {
+  if (!field) {
+    alert('This field is required!');
+    return 'error';
+  } else if (field.length >= 191) {
+    alert('Maximum length is 191 characters!');
+    return 'error';
+  }
+
+  return;
+};
+
 const addTodoButton = document.querySelector('#add-btn');
 const todoInput = document.querySelector('#todo-input');
-const list = document.querySelector('#list');
+const list = document.querySelector('#todo-list');
 
 addTodoButton.addEventListener('click', () => {
   const todo = todoInput.value;
-  
-  if (!todo) {
-    return;
-  }
 
-  const item = document.createElement('DIV');
+  if (validationField(todo)) return;
+
+  const item = document.createElement('div');
   item.classList.add('item');
 
-  const itemText = document.createElement('DIV');
+  const itemText = document.createElement('div');
   itemText.classList.add('item-text');
   itemText.textContent = todo;
 
-  const editInput = document.createElement('INPUT');
+  const editInput = document.createElement('input');
   editInput.classList.add('edit-input');
   editInput.classList.add('hide');
   editInput.name = 'edit-input';
   editInput.type = 'text';
   editInput.value = todo;
 
-  const editInputButton = document.createElement('BUTTON');
-  editInputButton.textContent = "UPDATE";
+  const editInputButton = document.createElement('button');
+  editInputButton.textContent = "✓";
   editInputButton.classList.add('action-btn');
   editInputButton.classList.add('update-btn');
   editInputButton.classList.add('hide');
   editInputButton.type = 'button';
 
-  const actionButtons = document.createElement('DIV');
+  const actionButtons = document.createElement('div');
   actionButtons.classList.add('action-btns');
 
-  const editButton = document.createElement('BUTTON');
+  const editButton = document.createElement('button');
   editButton.classList.add('action-btn');
   editButton.classList.add('edit-btn');
-  editButton.textContent = 'EDIT';
+  editButton.textContent = '✎';
 
   editButton.addEventListener('click', () => {
     editInput.classList.remove('hide');
     itemText.classList.add('hide');
     editInputButton.classList.remove('hide');
+
     editInputButton.addEventListener('click', () => {
-      itemText.textContent = editInput.value,
+      const editInputValue = editInput.value;
+      if (validationField(editInputValue)) return;
+
+      itemText.textContent = editInputValue,
       editInput.classList.add('hide');
       itemText.classList.remove('hide');
       editInputButton.classList.add('hide');
     })
   })
 
-  const removeButton = document.createElement('BUTTON');
+  const removeButton = document.createElement('button');
   removeButton.classList.add('action-btn');
   removeButton.classList.add('remove-btn');
-  removeButton.textContent = 'REMOVE';
+  removeButton.textContent = '❌';
 
   removeButton.addEventListener('click', () => {
     item.parentNode.removeChild(item);
